b59f7cba60ba66b6c075064cfa4f1006aced41a5
tarenkovia <tarenkov_ivan_00@mail.ru>
2023-04-23 14:21:55 +0400

    Добавлено исключение

--- src/Citizen.java
+++ src/Citizen.java
@@ -1,11 +1,17 @@
 public class Citizen {
     private String name;
-    public Citizen(String name, Government government) {
+    private int age;
+    public Citizen(String name, Government government, int age) {
         this.name = name;
+        this.age = age;
         government.registerCitizen(this);
     }
 
     void update(String News) {
         System.out.println(name + " узнали о новом указе - " + News);
+    }
+
+    public int getAge(){
+        return age;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void RegistrationNewCitizen(CityInAmerica city){
+        int age = 0;
+        Scanner in = new Scanner(System.in);
+        var cur = in.nextLine();
+
+        try {
+            age = Integer.valueOf(cur);
+        }
+        catch (NumberFormatException ce){
+            ce.getMessage();
+            Citizen third = new Citizen("Ivan", city, 30);
+            System.out.println(third.getAge());
+        }
+        if(age != 0){
+            Citizen fouth = new Citizen("Ivan", city, age);
+            System.out.println(fouth.getAge());
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Где-то из громофона на улицу - ");
         System.out.println("'Правительство подписало новый указ, согласну которому...'");
         System.out.println();
         var city = new CityInAmerica();
 
-        new Citizen("Уинстон", city);
-        new Citizen("Джулия", city);
+        Citizen first = new Citizen("Уинстон",  city, 30);
+        Citizen second = new Citizen("Джулия",city, 29);
+        RegistrationNewCitizen(city);
+
 
         city.setNews("'Сегодня Океания наш враг, Евразия наш союзник!'");
 
